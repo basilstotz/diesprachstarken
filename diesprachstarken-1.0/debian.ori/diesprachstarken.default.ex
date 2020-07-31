9f9e16621ab5bfc041e46269df7cf9703ee9ad9e
Basil Stotz <basil.stotz@gmail.com>
2020-07-31 12:04:44 +0200

    new commit

---
+++ diesprachstarken-1.0/debian.ori/diesprachstarken.default.ex
@@ -0,0 +1,10 @@
+# Defaults for diesprachstarken initscript
+# sourced by /etc/init.d/diesprachstarken
+# installed at /etc/default/diesprachstarken by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
